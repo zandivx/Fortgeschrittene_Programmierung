f3a583dc5608c962c71216894ae9f4d0fed3747a
zandivx <andreas.zach1@gmail.com>
2022-05-25 12:34:35 +0200

    Same logic in python works, so it is a C problem

--- numericalodes/RungeKutta4Vector.py
+++ numericalodes/RungeKutta4Vector.py
@@ -6,39 +6,62 @@ import matplotlib.pyplot as plt
 
 def RK4vec(func: list, n: int, t0: float, tmax: float, y0: list, h: float):
     size = math.ceil((tmax - t0) / h)
+    # print(f"{size=}")
     
-    # vectors
-    t = np.empty(size)
+    # vectors and matrix
     k1 = np.empty(n)
     k1 = np.empty(n)
     k2 = np.empty(n)
     k3 = np.empty(n)
     k4 = np.empty(n)
-    
-    # matrix
-    y = np.empty((n, size))
+    t = np.empty(size)
+    y = np.empty((size, n))
     
     # initialize arrays
     t[0] = t0
-    y[:,0] = y0
-    
-    
-    m = 1  # axis 0
-    i = 0  # axis 1
+    t[size-1] = tmax
+    y[0] = y0
+        
+    # axis 0
+    i = 1
     while i < size:
+        # print(f"{i=}")
+        
+        row = y[i-1]
+        
+        # axis 1 loops
+        m = 0
         while m < n:
-            k1[m] = func[m](t[i-1], *(y[:,i-1]))
-            k2[m] = func[m](t[i-1] + h/2, *(y[:,i-1] + h * float(k1[m]) / 2))
-            k3[m] = func[m](t[i-1] + h/2, *(y[:,i-1] + h * float(k2[m]) / 2))
-            k4[m] = func[m](t[i-1] + h, *(y[:,i-1] + h * float(k3[m])))
-
-            t[i] = t[i-1] + h
-            y[m][i] = y[m][i-1] + h/6 * (k1[m] + 2 * (k2[m] + k3[m]) + k4[m])
-
-            print(f"{m=}")
+            # print(f"k1: {i=}, {m=}")
+            k1[m] = func[m](t[i-1], *(row))
             m += 1
             
-        print(f"{i=}")
+        m = 0
+        while m < n:
+            # print(f"k2: {i=}, {m=}")
+            k2[m] = func[m](t[i-1] + h/2, *(row + h/2 * k1))
+            m += 1
+            
+        m = 0
+        while m < n:
+            # print(f"k3: {i=}, {m=}")
+            k3[m] = func[m](t[i-1] + h/2, *(row + h/2 * k2))
+            m += 1
+            
+        m = 0
+        while m < n:
+            # print(f"k4: {i=}, {m=}")
+            k4[m] = func[m](t[i-1] + h, *(row + h * k3))
+            m += 1
+            
+        m = 0
+        while m < n:
+            # print(f"m: {i=}, {m=}")
+            y[i,m] = y[i-1,m] + h/6 * (k1[m] + 2*(k2[m] + k3[m]) + k4[m])
+            m += 1
+            
+        t[i] = t[i-1] + h
+        
         i += 1
         
     return t, y
@@ -49,12 +72,12 @@ def phi_dot(t, phi, omega):
 def omega_dot(t, phi, omega):
     return -math.sin(phi)
 
+if "__name__" == "__main__":
+    # t1, y1 = RK4vec([lambda t, y: y], 1, 0, 10, [1], 0.1)
+    t2, y2 = RK4vec([phi_dot, omega_dot], 2, 0, 10, [math.pi/8, 0], 0.1)
 
-# t1, y1 = RK4vec([lambda t, y: y], 1, 0, 10, [1], 0.1)
-t2, y2 = RK4vec([phi_dot, omega_dot], 2, 0, 10, [math.pi/8, 0], 0.1)
+    print(t2)
+    print(y2)
 
-print(t2)
-print(y2)
-
-plt.plot(t2, y2[1])
-plt.savefig("plot.pdf")
+    plt.plot(t2, y2[:,0])
+    plt.savefig("output/python_plot.pdf")
--- numericalodes/test.py
+++ numericalodes/test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from RungeKutta4Vector import RK4vec, phi_dot, omega_dot
 
 df = pd.read_csv("output/game3.csv")
 t = np.arange(0, 16*np.pi, 1e-4)
@@ -14,7 +15,9 @@ def system(t, y):
 sol = solve_ivp(system, (0, 16*np.pi), [np.pi*0.9, 0], rtol=1e-4)
 plt.plot(sol.t, sol.y[0], "--", label=f"solve_ivp")
 
+t_py, y_py = RK4vec([phi_dot, omega_dot], 2, 0, 16*np.pi, [np.pi*0.9, 0], 1e-4)
+plt.plot(t_py, y_py[:,0], "-.", label="self_py")
 
-plt.plot(t, df["y0"], label="self")
+plt.plot(t, df["y0"], label="self_c")
 plt.legend()
 plt.savefig("output/game3.pdf")
